016e41e02914ef7d4924094d43a4fa79e31d2bf3
BigBielNunes <99697995+BigBielNunes@users.noreply.github.com>
2023-09-11 11:35:09 -0300

    Update main.c

--- atividade4/main.c
+++ atividade4/main.c
@@ -11,21 +11,17 @@ int main(int argc, char *argv[]) {
     int num_elements = argc - 1;
     int *numbers = (int *)malloc(num_elements * sizeof(int));
 
-    // Converte os argumentos de linha de comando em números inteiros
     for (int i = 1; i < argc; i++) {
         numbers[i - 1] = atoi(argv[i]);
     }
 
-    // Chama a função bubble_sort para ordenar os números
     bubble_sort(numbers, num_elements);
 
-    // Imprime a lista ordenada
     for (int i = 0; i < num_elements; i++) {
         printf("%d ", numbers[i]);
     }
     printf("\n");
 
-    // Libera a memória alocada para o array de números
     free(numbers);
 
     return 0;
